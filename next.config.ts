d738ce96f6a9625c4124f06fd4e761e3c00de0fc
Marcelo Kopmann <marcelokopmann@gmail.com>
2025-08-26 00:24:35 -0300

    fix: next image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Allow external avatar images served by Google
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
 };
 
 export default nextConfig;
